9a32605fc0b24d1d9f97f10ad93f54f5783a3e98
minb__m <kmb3322@naver.com>
2025-01-08 20:32:10 +0900

    스와이프 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,7 @@
-// components/PostCard.tsx
-
 import { Box, Image as ChakraImage, Flex, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { FC, useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable"; // 스와이프 이벤트 감지 라이브러리
 import { getPhotoMetadata, PhotoMetadata } from "../utils/getPhotoMetadata";
 import { MapComponent } from "./MapComponent";
 
@@ -16,30 +15,27 @@ interface PostCardProps {
   username: string;
   location: string;
   images: string[];
-  //onClick: () => void; // 클릭 이벤트 핸들러
 }
 
 const MotionBox = motion(Box);
 
 const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images }) => {
   const [imageMetadata, setImageMetadata] = useState<PhotoMetadata>({
-    date:  null,
+    date: null,
     latitude: null,
-  longitude: null,
-
-  country: null,  // 역지오코딩 결과
-  city: null,
-  state: null,
-  postalCode:  null,
-  street: null,
-  displaySrc: "",  // (HEIC 변환 시 사용) 최종 표시용 이미지 URL
-
+    longitude: null,
+    country: null,
+    city: null,
+    state: null,
+    postalCode: null,
+    street: null,
+    displaySrc: "",
   });
 
   useEffect(() => {
     async function loadImageMetadata() {
       if (images && images.length > 0) {
-        const metadata = await getPhotoMetadata(images[0]); // 첫 번째 이미지의 메타데이터 사용
+        const metadata = await getPhotoMetadata(images[0]);
         setImageMetadata(metadata);
       }
     }
@@ -52,6 +48,13 @@ const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images 
       ? { lat: imageMetadata.latitude, lng: imageMetadata.longitude }
       : undefined;
 
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => console.log("Swiped left"),
+    onSwipedRight: () => console.log("Swiped right"),
+    preventDefaultTouchmoveEvent: true,
+    trackTouch: true,
+  });
+
   return (
     <MotionBox
       bg="white"
@@ -61,18 +64,17 @@ const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images 
       cursor="pointer"
       whileHover={{ scale: 1.02, boxShadow: "lg" }}
       whileTap={{ scale: 0.98 }}
-      //onClick={onClick} // 클릭 시 부모에게 전달
     >
-      {/* 프로필 및 위치 */}
       <Flex alignItems="center" mb={4}>
         <ChakraImage src={profileImage} alt={username} boxSize="50px" borderRadius="full" mr={3} />
         <Box>
           <Text fontWeight="bold">{username}</Text>
-          <Text fontSize="sm" color="gray.500">{location}</Text>
+          <Text fontSize="sm" color="gray.500">
+            {location}
+          </Text>
         </Box>
       </Flex>
 
-      {/* 사진들 - 가로 스크롤 */}
       <Box
         height="150px"
         display="flex"
@@ -82,8 +84,9 @@ const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images 
         css={{
           scrollSnapType: "x mandatory",
           WebkitOverflowScrolling: "touch",
-          '&::-webkit-scrollbar': { display: 'none' },
+          "&::-webkit-scrollbar": { display: "none" },
         }}
+        {...swipeHandlers} // 스와이프 이벤트 핸들러 추가
       >
         {images.map((image, index) => (
           <Box
@@ -98,7 +101,7 @@ const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images 
           >
             <ChakraImage
               src={image}
-             alt={`Scrollable Image ${index + 1}`}
+              alt={`Scrollable Image ${index + 1}`}
               objectFit="cover"
               width="100%"
               height="100%"
@@ -106,13 +109,13 @@ const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images 
           </Box>
         ))}
       </Box>
-      {/* 지도 (비활성화) */}
+
       <Box mb={40} height="200px">
         {coordinates ? (
           <MapComponent location={location} coordinates={coordinates} isInteractive={false} />
         ) : (
           <ChakraImage
-            src="/images/dummy_map_image.jpg" // 대체 지도 이미지
+            src="/images/dummy_map_image.jpg"
             alt="Dummy Map"
             objectFit="cover"
             w="100%"
@@ -121,8 +124,6 @@ const PostCard: FC<PostCardProps> = ({ profileImage, username, location, images 
           />
         )}
       </Box>
-
-      
     </MotionBox>
   );
 };
