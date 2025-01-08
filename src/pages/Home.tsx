486b9c175df6011cd1d49269d37039895ea59b6b
minb__m <kmb3322@naver.com>
2025-01-08 20:52:19 +0900

    한번더

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -101,7 +101,7 @@ export default function Home() {
       setTimeout(() => {
         setShowWelcomeAnimation(false);
         localStorage.setItem("first_login", "false");
-      }, 6000);
+      }, 7000);
     }
     fetchGroups();
   }, []);
